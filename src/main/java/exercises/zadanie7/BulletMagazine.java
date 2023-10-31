e365caa7e3dded7fa1d22d9587810db4ccbafee9
orgulec <oggilion@interia.pl>
2023-10-31 20:25:34 +0100

    Initial commit

---
+++ src/main/java/exercises/zadanie7/BulletMagazine.java
@@ -0,0 +1,32 @@
+package exercises.zadanie7;
+
+import java.util.Stack;
+
+public class BulletMagazine {
+    private Stack<String> capacity;
+    private int magazineSize;
+
+    BulletMagazine(int magazineSize){
+        this.capacity = new Stack<String>();
+        this.magazineSize = magazineSize;
+    }
+    public void loadBullet(String bullet){
+        if(capacity.size()<magazineSize) {
+            capacity.add(bullet);
+        } else{
+            System.out.println("Magazine is full.");
+        }
+    }
+    public void isLoaded(){
+        System.out.println("Magazine status: "+capacity.size()+"/"+magazineSize);
+    }
+    public void shot(){
+        if(capacity.size()>0) {
+            System.out.println(capacity.lastElement());
+            capacity.remove(capacity.lastElement());
+        }else{
+            System.out.println("Magazine is empty!");
+        }
+    }
+
+}
---
+++ src/main/java/exercises/zadanie7/Gun.java
@@ -0,0 +1,37 @@
+package exercises.zadanie7;
+/*
+Zadanie 7.
+Stwórz klasę imitującą magazynek do broni. Klasa powinna mieć możliwość definiowania rozmiaru magazynka za pomocą konstruktora.
+Zaimplementuj metody:
+loadBullet(String bullet) → dodawanie naboju do magazynka, nie pozwala załadować więcej naboi niż wynosi pojemność magazynka
+isLoaded() → zwraca informację o tym czy broń jest naładowana (przynajmniej jeden nabój) czy nie
+shot() → jedno wywołanie wystrzeliwuje (wypisuje w konsoli wartość string) jeden - ostatni załadowany nabój
+i przygotowuje kolejny, załadowany przed ostatnim, jeżeli nie ma więcej nabojów to wypisuje w konsoli “pusty magazynek”
+ */
+public class Gun {
+    public static void main(String[] args) {
+
+        BulletMagazine gun1 = new BulletMagazine(4);
+
+        gun1.isLoaded();
+
+        gun1.loadBullet("Bang 1!");
+        gun1.loadBullet("Bang 2!");
+        gun1.loadBullet("Bang 3!");
+        gun1.loadBullet("Bang 4!");
+        gun1.loadBullet("Bang 5!");
+
+        gun1.isLoaded();
+
+        gun1.shot();
+        gun1.shot();
+        gun1.shot();
+        gun1.shot();
+        gun1.shot();
+
+        gun1.isLoaded();
+    }
+
+
+
+}
